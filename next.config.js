c1d8a7c2bc970c9d896b0d93e8174063a4e11cc0
sm3522352 <smuuuzik1@gmail.com>
2025-10-04 17:11:23 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true
-  },
-  images: {
-    // Domains can be updated to include external image sources when needed.
-    domains: []
-  }
+  swcMinify: true
 };
 
 module.exports = nextConfig;
